f076bbf39a40459d10bc2d50873db6370df240d8
Shane <sfbreining@gmail.com>
2020-01-25 22:38:50 -0800

    Updated the migration script to include a couple more columns. Might as well keep track of all holidays. Also started building out the model for that table and doing manual testing.

--- utility/models/punch.py
+++ utility/models/punch.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Punch:
     """A class that will handle Database interactions with the holidays table. 
 
@@ -6,10 +9,27 @@ class Punch:
     _connection : Database
         A connection to the Database.
 
+    _id : int
+        The ID for the row that should be updated for a given day.
+
     Methods
     -------
+    get_punch_by_id(id_)
+        Get the punch by the ID of the row.
+
+    in_(datetime)
+        Inserts the row, and updates the clock in time.
+
+    start(datetime)
+        Updates the punch card to have the time lunch started.
+
+    end(datetime)
+        Updates the punch card to have the time lunch ended.
+
+    out(datetime)
+        Updates the punch card to have the time the day ended.
     """
-    
+
     def __init__(self, connection):
         """
         Creates a new instance of the Punch model object.
@@ -20,3 +40,69 @@ class Punch:
             Holds on to database connection.
         """
         self._connection = connection
+        self._id = 0
+
+    def get_punch_by_id(self, id_):
+        """
+        Retrieves the punch row by the id.
+
+        Parameters
+        ----------
+        id_ : int, required
+            The ID of the row to retrieve.
+
+        Returns
+        -------
+        tuple
+            The row, an empty tuple on failure.
+        """
+        sql = 'SELECT * FROM punches WHERE id=?'
+        data = (id_,)
+
+        try:
+            self._connection.execute(sql, data)
+        except sqlite3.OperationalError:
+            return ()
+
+        results = self._connection.fetchall()
+
+        if not results:
+            return ()
+
+        return results[0]
+
+    def in_(self, datetime):
+        """
+        Will insert a new row with the datetime provided as the punch in.
+
+        Parameters
+        ----------
+        datetime : datetime, required
+            The datetime to record the punch in.
+
+        Returns
+        -------
+        bool
+            True on successful insert.
+        """
+        sql = 'INSERT INTO punches(clock_in) VALUES(?)'
+        data = (datetime,)
+
+        try:
+            self._connection.execute(sql, data)
+            self._connection.commit()
+        except sqlite3.OperationalError:
+            return False
+
+        self._id = self._connection.get_last_row_id()
+
+        return True
+
+    def start(self, datetime):
+        pass
+
+    def end(self, datetime):
+        pass
+
+    def out(self, datetime):
+        pass
