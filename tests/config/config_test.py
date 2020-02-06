29f555eee7e3f05f823d50c694c7a86f9dbe393e
Shane <sfbreining@gmail.com>
2020-02-05 20:14:30 -0800

    Style changes. That's it. Should have done more. But I didn't. So style changes are what this commit is about.

--- tests/config/config_test.py
+++ tests/config/config_test.py
@@ -1,23 +1,30 @@
 def test_get_login_returns_credentials():
-  pass
+    pass
+
 
 def test_get_login_url_returns_url_string():
-  pass
+    pass
+
 
 def test_get_dashboard_url_returns_url_string():
-  pass
+    pass
+
 
 def test_get_implicit_wait_returns_number_of_seconds():
-  pass
+    pass
+
 
 def test_get_db_path_returns_file_path_to_dotdb_file():
-  pass
+    pass
+
 
 def test_get_questions_returns_dictionary_of_questions_answers():
-  pass
+    pass
+
 
 def test_get_pager_duty_info_returns_dictionary():
-  pass
+    pass
+
 
 def test_get_start_hour_returns_number():
-  pass
+    pass
