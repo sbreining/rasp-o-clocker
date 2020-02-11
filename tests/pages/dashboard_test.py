ebbbb405dda1300305c16ccee64558ab7c24d8e2
sbreining <sbreining@paylease.com>
2020-02-10 16:16:45 -0800

    Removed some code duplication. I would still like to data provide it, but for now, this will be fine.

--- tests/pages/dashboard_test.py
+++ tests/pages/dashboard_test.py
@@ -27,36 +27,28 @@ def test_clock_in_calls_click_and_nav_away(dash_and_action):
     dash, action, driver = dash_and_action
     dash.clock_in()
 
-    driver.find_element_by_name.assert_called_once()
-    action.click.assert_called_once()
-    driver.get.assert_called_once()
+    _run_action_assertions(action, driver)
 
 
 def test_start_lunch_calls_click_and_nav_away(dash_and_action):
     dash, action, driver = dash_and_action
     dash.start_lunch()
 
-    driver.find_element_by_name.assert_called_once()
-    action.click.assert_called_once()
-    driver.get.assert_called_once()
+    _run_action_assertions(action, driver)
 
 
 def test_end_lunch_calls_click_and_nav_away(dash_and_action):
     dash, action, driver = dash_and_action
     dash.end_lunch()
 
-    driver.find_element_by_name.assert_called_once()
-    action.click.assert_called_once()
-    driver.get.assert_called_once()
+    _run_action_assertions(action, driver)
 
 
 def test_clock_out_calls_click_and_nav_away(dash_and_action):
     dash, action, driver = dash_and_action
     dash.clock_out()
 
-    driver.find_element_by_name.assert_called_once()
-    action.click.assert_called_once()
-    driver.get.assert_called_once()
+    _run_action_assertions(action, driver)
 
 
 def test_go_to_pto_clicks_element_and_returns_page_object():
@@ -73,3 +65,9 @@ def test_go_to_pto_clicks_element_and_returns_page_object():
     driver.find_element_by_xpath.assert_called_once_with('//a[text()="Launch Time & Attendance"]')
     mock_element.click.assert_called_once()
     assert isinstance(page, PaidTimeOff)
+
+
+def _run_action_assertions(action, driver):
+    driver.find_element_by_name.assert_called_once()
+    action.click.assert_called_once()
+    driver.get.assert_called_once()
