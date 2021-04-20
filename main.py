80aaddf75631aaa39e7d32837c8248235f846245
Shane.Breining <shane.breining@kount.com>
2021-04-20 12:41:01 -0600

    Remove GUI And Update Dependencies
    I am no longer an hourly employee. This project was to simplify my
    efforts of being hourly by clocking me in and out. It was intended to
    user with a Raspberry Pi. I further wanted to add a GUI component to the
    proejct, and make it user friendly to all hourly employees. I have since
    abandoned that idea.
    
    Now, Github notified me of a potential dependency vulnerability. So, I
    updated all versions possible, and made sure the tests still run.
    However, I did ensure that the software works. Notably because I cannot
    run any manual testing.

--- main.py
+++ main.py
@@ -13,60 +13,49 @@ from sys import exc_info
 
 
 def main() -> None:
-    # # Load up environment configuration
+    # Load up environment configuration
     load_dotenv(join(dirname(__file__), 'data', '.env'))
     config = Config()
-    #
-    # # Connect to the database
-    # db = Database(config)
-    #
-    # # Add the connection to the two models.
-    # holiday = Holiday(db)
-    # punch = Punch(db)
-    #
-    # # Set the options to have chrome be headless
-    # op = Options()
-    # op.add_argument('--headless')
-    #
-    # # Instantiate the driver
-    # driver = Chrome(options=op)
-    # driver.implicitly_wait(config.get_implicit_wait())
-    #
-    # # Instantiate the pager
+    
+    # Connect to the database
+    db = Database(config)
+    
+    # Add the connection to the two models.
+    holiday = Holiday(db)
+    punch = Punch(db)
+    
+    # Set the options to have chrome be headless
+    op = Options()
+    op.add_argument('--headless')
+    
+    # Instantiate the driver
+    driver = Chrome(options=op)
+    driver.implicitly_wait(config.get_implicit_wait())
+    
+    # Instantiate the pager
     smtp = SMTP_SSL(GMAIL_DOMAIN)
     pager = PagerDuty(config, smtp)
-    #
-    # # Set the args in a dictionary for future use
-    # args = {
-    #     'config': config,
-    #     'driver': driver,
-    #     'holiday': holiday,
-    #     'pager': pager,
-    #     'punch': punch
-    # }
+    
+    # Set the args in a dictionary for future use
+    args = {
+        'config': config,
+        'driver': driver,
+        'holiday': holiday,
+        'pager': pager,
+        'punch': punch
+    }
 
+    punch_card_manager = PunchCardManager(args)
+    
     try:
-        GUI().build().start()
+        # Start the process manager
+        punch_card_manager.start()
     except:
         exception_type, value = exc_info()[:2]
         pager.alert(
             'PROGRAM CRASH, needs restart.\nException - %s\nValue - %s' %
             (exception_type, value)
         )
-
-    # # TODO: Remove below this line, make it part of GUI.
-    #
-    # punch_card_manager = PunchCardManager(args)
-    #
-    # try:
-    #     # Start the process manager
-    #     punch_card_manager.start()
-    # except:
-    #     exception_type, value = exc_info()[:2]
-    #     pager.alert(
-    #         'PROGRAM CRASH, needs restart.\nException - %s\nValue - %s' %
-    #         (exception_type, value)
-    #     )
 
 
 if __name__ == "__main__":
--- src/gui/components/button.py
+++
@@ -1,7 +0,0 @@
-from tkinter import Button as TkButton
-
-
-class Button(TkButton):
-    def __init__(self, window, button_text, command, pack_side):
-        super().__init__(window, text=button_text, command=command)
-        self.pack(side=pack_side)
